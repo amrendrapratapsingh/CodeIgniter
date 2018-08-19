c0be9ccac2d0830d7fee0ef3ea94b2380517193a
Amrendra Pratap Singh <amrendrapratapcoders@gmail.com>
2018-08-19 11:26:15 +0530

    First_view_files...

---
+++ my_first_view.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Welcome to My first_view</title>
+<style>
+body{
+	background-color:orangered;
+	padding:10px;
+	margin:0;
+	
+}
+div{
+	background-color:blue;
+	text-align:center;
+	
+	font-size:20px;
+	color:white;
+}
+</style>
+</head>
+<body>
+<div>
+<h2>This is my first_view files in codeigniter.</h2>
+</div>
+</body>
+</html>
